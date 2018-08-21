f1c7572c70a46ee5fce97e364d07079dd93e0566
TEN-PEARL.COM\kashif.atiq <kashif.atiq@tenpearls.com>
2018-08-21 16:43:21 +0500

    2

--- ImagesToDatabase/Form1.Designer.cs
+++ ImagesToDatabase/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtSelectedFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSelectedFolder
+            // 
+            this.txtSelectedFolder.Location = new System.Drawing.Point(13, 13);
+            this.txtSelectedFolder.Name = "txtSelectedFolder";
+            this.txtSelectedFolder.Size = new System.Drawing.Size(660, 20);
+            this.txtSelectedFolder.TabIndex = 0;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(690, 11);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(146, 23);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Select Folder to upload";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.Location = new System.Drawing.Point(0, 91);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1032, 398);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(379, 48);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(145, 27);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 489);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtSelectedFolder);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtSelectedFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- ImagesToDatabase/Form1.cs
+++ ImagesToDatabase/Form1.cs
@@ -3,18 +3,94 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ImagesToDatabase.DB;
+using ImageDb = ImagesToDatabase.DB.Image;
 namespace ImagesToDatabase
 {
     public partial class Form1 : Form
     {
+        DB.ImagesToDatabaseEntities _EF = new ImagesToDatabaseEntities();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Add items in the listview
+                string[] arr = new string[2];
+                ListViewItem listViewItem;
+
+                folderBrowserDialog1.ShowDialog();
+                txtSelectedFolder.Text = folderBrowserDialog1.SelectedPath;
+                foreach (string fileName in Directory.EnumerateFiles(txtSelectedFolder.Text.Trim(), "*.*", SearchOption.AllDirectories))
+                {
+                    arr[0] = fileName;
+                    arr[1] = Path.GetFileName(fileName);
+                    listViewItem = new ListViewItem(arr);
+
+                    bool isImageExists = _EF.Images.Any(r => r.FilePath == fileName);
+                    if(isImageExists)
+                    {
+                        listViewItem.Checked = false;
+                        listViewItem.BackColor = Color.LightGreen;
+                    }
+                    else
+                    {
+                        listViewItem.Checked = false;
+                    }
+                    listView1.Items.Add(listViewItem);
+                    var dd3 = from im in _EF.Images select im;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            //Add column header
+            listView1.Columns.Add("File", 500);
+            listView1.Columns.Add("File Name", 500);
+        }
+
+        public byte[] GetImageData(string filePath)
+        {
+            FileStream fsImageStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            byte[] bImageData = new byte[fsImageStream.Length];
+            fsImageStream.Read(bImageData, 0, System.Convert.ToInt32(fsImageStream.Length));
+            fsImageStream.Close();
+            return bImageData;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem lstItem in listView1.Items)
+            {
+                if(lstItem.Checked)
+                {
+                    ImageDb newImage = new ImageDb();
+                    newImage.DateCreated = DateTime.Now;
+                    newImage.FileName = lstItem.SubItems[1].Text;
+                    newImage.FilePath = lstItem.SubItems[0].Text;
+                    newImage.ImageBytes = GetImageData(newImage.FilePath);
+                    _EF.Images.Add(newImage);
+                    _EF.SaveChanges();
+                }
+            }
         }
     }
 }
